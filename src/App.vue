482a6e07de2a6d905f8bd6f67074f6185466ff00
zatbezi <165803209+zatbezi@users.noreply.github.com>
2024-04-03 05:31:28 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!-- Ubah src untuk menunjuk ke gambar yang ingin Anda tambahkan -->
+    <img
+      alt="Foto"
+      class="foto"
+      src="@/assets/image/os3b_dccv_140923-removebg-preview.png"
+      width="300"
+      height="200"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="Hi it's me Azizi" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+/* Ganti .logo dengan .foto */
+.foto {
+  display: block;
+  margin: 0 auto;
+  height: 300px;
+  widht: 400px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>About Me</template>
+    I am a student at the Islamic University of Riau, located on Kaharuddin Nst Street in Pekanbaru.
+    Currently, I am in the 4th semester of the Computer Engineering department at the Islamic
+    University of Riau. I come from Rokan Hilir District, Bagan Sinembah Subdistrict, Riau Province.
+  </WelcomeItem>
+  <hr />
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Education</template>
+
+    SDN 1 BATU HAMPAR 2009-2015<br />
+    SMPN 1 BATU HAMPAR 2015-2018<br />
+    SMAN 1 BATU HAMPAR 2018-2021<br />
+    ISLAMIC UNIVERSITY OF RIAU 2022-NOW
+  </WelcomeItem>
+  <hr />
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Skills</template>
+    I have skills in programming, especially in JavaScript and the Vue.js framework. Additionally, I
+    am also familiar with HTML, CSS, and various other web technologies. I am confident in
+    problem-solving and have good communication skills.
+  </WelcomeItem>
+  <hr />
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Instagram</template>
+    <h2>Instagram</h2>
+    <p>
+      You can view my Instagram
+      <a
+        href="https://www.instagram.com/zatbezi?igsh=MTB5enBxMzdqYW1heQ=="
+        target="_blank"
+        rel="noopener"
+        >@zatbezi</a
+      >, where you can follow me.
+    </p>
+  </WelcomeItem>
+  <hr />
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Contact Me</template>
+
+    <p>
+      If you wish to contact me, please send an email to
+      <a href="mailto:mhmmdazizi18@gmail.com" target="_blank" rel="noopener"
+        >mhmmdazizi18@gmail.com</a
+      >.
+    </p>
+  </WelcomeItem>
+</template>
